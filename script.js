19b2f5f14635b472c5b32981140f03382ca0b8fa
Juaquin Andres <100443432+juakolp25@users.noreply.github.com>
2022-04-21 13:08:30 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
 const btnToggle = document.getElementById('btn-toggle');
 const lineToggle = document.querySelectorAll('.line');
 const menuResponsive = document.getElementById('menu-list_responsive');
+const toggleTheme = document.getElementById('toggle-theme');
+const toggleIcon = document.getElementById('toggle-icon');
+const toggleText = document.getElementById('toggle-text');
 
 btnToggle.addEventListener('click', ()=>{
     lineToggle.forEach(x => x.classList.toggle('toggle-line_cruz'));
     menuResponsive.classList.toggle('link-menu_responsive-visible');
 });
+
+
+/* <-- Seccion de los temas de la pag --> */
+
+toggleTheme.addEventListener('click', ()=>{
+    document.body.classList.toggle("light");
+    if(toggleIcon.src.includes("imagenes/moon-dark.svg")){
+        toggleIcon.src = 'imagenes/mode-sun.svg';
+        toggleText.textContent = 'Light Mode';
+    }else{
+        toggleIcon.src = 'imagenes/moon-dark.svg';
+        toggleText.textContent = 'Dark Mode';
+    }
+})
